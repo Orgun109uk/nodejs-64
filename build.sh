782480d247f47c9055c36605827c81828edc0f00
Orgun109uk <orgun109uk@gmail.com>
2015-09-09 21:59:18 +0100

    Updated errors in nodejs install script.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 export ATLAS_TOKEN="A2208a5Eg1xA2w.atlasv1.wm2A5sSbMTRgOUBi1I9arYMbxnOz6HWJAy2RFbziE5eLd8s5xcS5u8DYeF2M9vvmmTg"
 
-../../packer/packer push -name Orgun109uk/NodeJS-64 template.json
+../packer/packer push -name Orgun109uk/NodeJS-64 template.json
--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -21,16 +21,13 @@ echo "" >> /home/vagrant/.bashrc
 nvm install 0.12
 nvm alias default 0.12
 
-chown vagrant:vagrant $NVM_DIR -R
-
-chown vagrant:vagrant /home/vagrant/.npm -R
-chown vagrant:vagrant /home/vagrant/.npm/_locks -R
-
 echo "nvm use default" >> /root/.bashrc
 echo "" >> /root/.bashrc
 
 echo "nvm use default" >> /home/vagrant/.bashrc
 echo "" >> /home/vagrant/.bashrc
+
+NODE_VERSION=$(node --version)
 
 #################################################
 # Install Grunt CLI
@@ -50,8 +47,8 @@ apt-get -y install mongodb-org
 echo "Installing mongo-express..."
 npm install -g mongo-express
 
-cp /usr/local/nvm/versions/node/v0.12.2/lib/node_modules/mongo-express/config.default.js /usr/local/nvm/versions/node/v0.12.2/lib/node_modules/mongo-express/config.js
-sed -i.bak 's|^  useBasicAuth: true,|  useBasicAuth: false,|' /usr/local/nvm/versions/node/v0.12.2/lib/node_modules/mongo-express/config.js
+cp /usr/local/nvm/versions/node/v0.12.2/lib/node_modules/mongo-express/config.default.js /usr/local/nvm/versions/node/${NODE_VERSION}/lib/node_modules/mongo-express/config.js
+sed -i.bak 's|^  useBasicAuth: true,|  useBasicAuth: false,|' /usr/local/nvm/versions/node/${NODE_VERSION}/lib/node_modules/mongo-express/config.js
 
 touch /home/vagrant/startups/mongo-express
 chmod +x /home/vagrant/startups/mongo-express
@@ -76,3 +73,7 @@ echo "" >> /home/vagrant/startups/maildev
 echo "echo \"Starting mailDev...\"" >> /home/vagrant/startups/maildev
 echo "nohup maildev &>/dev/null &" >> /home/vagrant/startups/maildev
 echo "" >> /home/vagrant/startups/maildev
+
+chown vagrant:vagrant $NVM_DIR -R
+chown vagrant:vagrant /home/vagrant/.npm -R
+chown vagrant:vagrant /home/vagrant/.npm/_locks -R
