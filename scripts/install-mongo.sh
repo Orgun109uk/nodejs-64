eda770e69cc0f19de6d4855f3d6ef8b62e9efe2c
Orgun109uk <orgun109uk@gmail.com>
2015-09-12 11:41:20 +0100

    Resolved incorrect location for mongo express.

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -1,38 +1,6 @@
 #!/bin/bash -e
 
-#################################################
-# Install Node.js
-echo "Installing NodeJS and NPM..."
-curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | NVM_DIR=/usr/local/nvm bash
-
-export NVM_DIR="/usr/local/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-echo "" >> /root/.bashrc
-echo "export NVM_DIR=\"${NVM_DIR}\"" >> /root/.bashrc
-echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >> /root/.bashrc
-echo "" >> /root/.bashrc
-
-echo "" >> /home/vagrant/.bashrc
-echo "export NVM_DIR=\"${NVM_DIR}\"" >> /home/vagrant/.bashrc
-echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >> /home/vagrant/.bashrc
-echo "" >> /home/vagrant/.bashrc
-
-nvm install 0.12
-nvm alias default 0.12
-
-echo "nvm use default" >> /root/.bashrc
-echo "" >> /root/.bashrc
-
-echo "nvm use default" >> /home/vagrant/.bashrc
-echo "" >> /home/vagrant/.bashrc
-
-NODE_VERSION=$(node --version)
-
-#################################################
-# Install Grunt CLI
-echo "Installing Grunt CLI..."
-npm install -g grunt-cli
+nvm use default
 
 #################################################
 # Install MongoDB
@@ -64,24 +32,6 @@ chmod +x /home/vagrant/startups/mongo-express
 echo "#!/bin/bash -e" >> /home/vagrant/startups/mongo-express
 echo "" >> /home/vagrant/startups/mongo-express
 echo "echo \"Starting Mongo Express...\"" >> /home/vagrant/startups/mongo-express
-echo "cd /usr/local/nvm/versions/node/${NODE_VERSION}/node_modules/mongo-express" >> /home/vagrant/startups/mongo-express
+echo "cd /usr/local/nvm/versions/node/${NODE_VERSION}/lib/node_modules/mongo-express" >> /home/vagrant/startups/mongo-express
 echo "nohup node app.js &>/dev/null &" >> /home/vagrant/startups/mongo-express
 echo "" >> /home/vagrant/startups/mongo-express
-
-#################################################
-# Install MailDev
-echo "Installing maildev..."
-npm install -g maildev
-
-touch /home/vagrant/startups/maildev
-chmod +x /home/vagrant/startups/maildev
-
-echo "#!/bin/bash -e" >> /home/vagrant/startups/maildev
-echo "" >> /home/vagrant/startups/maildev
-echo "echo \"Starting mailDev...\"" >> /home/vagrant/startups/maildev
-echo "nohup maildev &>/dev/null &" >> /home/vagrant/startups/maildev
-echo "" >> /home/vagrant/startups/maildev
-
-chown vagrant:vagrant $NVM_DIR -R
-chown vagrant:vagrant /home/vagrant/.npm -R
-chown vagrant:vagrant /home/vagrant/.npm/_locks -R
