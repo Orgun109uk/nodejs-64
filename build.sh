829ce93184159e4cc9dec76a2cb3b489448f0dd1
Nic Ashton <orgun109uk@gmail.com>
2017-03-22 01:04:33 +0100

    Updated ignore file.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ATLAS_TOKEN="A2208a5Eg1xA2w.atlasv1.wm2A5sSbMTRgOUBi1I9arYMbxnOz6HWJAy2RFbziE5eLd8s5xcS5u8DYeF2M9vvmmTg"
-
-../packer/packer push -name Orgun109uk/NodeJS-64 template.json
